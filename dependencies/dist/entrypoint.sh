00b908957db6596f7a373e7696c778e89b8afa21
Mykyta Mykulskyi <81817136+Mik-813@users.noreply.github.com>
2026-01-03 23:46:02 +0100

    wip: setting up development and deployment

--- dependencies/dist/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-cd /workspace/dependencies
-
-exec nix develop ./shared --command php-fpm -F -y ./dist/php-fpm.conf
--- dependencies/shared/flake.nix
+++ dependencies/shared/flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "API development dependencies";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -10,7 +8,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
         php = pkgs.php82.buildEnv {
           extensions = ({ enabled, all }: enabled ++ (with all; [
             bcmath
@@ -27,19 +24,19 @@
             memory_limit = 512M
           '';
         };
+        deps = [ 
+          php
+          pkgs.php82Packages.composer
+          pkgs.nodejs
+        ];
       in
       {
+        packages.default = pkgs.buildEnv { 
+          name = "php-env";
+          paths = deps;
+        };
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            php
-            php82Packages.composer
-            nodejs
-          ];
- 
-          shellHook = ''
-            [ -n "$FLAKE_ENV" ] && exit
-            export FLAKE_ENV=1
-          '';
+          buildInputs = deps;
         };
       }
     );
