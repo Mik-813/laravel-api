40ac5ce19ab2f7c2a6ce65b18c8f57376ea911ac
Mykyta Mykulskyi <81817136+Mik-813@users.noreply.github.com>
2026-01-12 00:01:27 +0100

    feat: dependencies added, project generated

--- dependencies/dist/todo/nix-webserver-container/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-DIST="/workspace/dependencies/dist"
-
-# TODO: create entry point (run php-fpm and nginx)
-# or just php-fmp and nginx is going to be in another container
-# the pro
